781df43609b21897282f5aafb570ad5de3ae5806
Duane Mitchell <duane.a.mitchell@outlook.com>
2026-06-27 21:02:18 -0400

    created GetTotalExpense test

--- BudgetTracker/CalculationTest.cs
+++ BudgetTracker/CalculationTest.cs
@@ -1,15 +1,27 @@
 ﻿using NUnit.Framework;
 using BudgetTracker;
+using BudgetTracker.Core.Models;
 
 namespace BudgetTracker.Tests
 {
     public class CalculationTests
     {
+        private BudgetTracker tracker = new BudgetTracker();
+
         [Test]
         public void GetTotalIncome_NoTransactions_ReturnsZero()
         {
-            var tracker = new BudgetTracker();
             Assert.That(tracker.GetTotalIncome(), Is.EqualTo(0m));
+        }
+
+        [Test]
+        public void GetTotalExpenses()
+        {
+            tracker.AddTransaction(new Transaction(50m, "Groceries", "Food", DateTime.Today, TransactionType.Expense));
+            tracker.AddTransaction(new Transaction(30m, "Gas", "Transport", DateTime.Today, TransactionType.Expense));
+
+            Assert.That(tracker.GetTotalExpenses(), Is.EqualTo(80m));
+
         }
     }
 }
